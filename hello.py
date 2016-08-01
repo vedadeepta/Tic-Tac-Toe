0d28e1b5a5a722587655607825c20cc8cebb74d2
veda <pal.veda.deepta@gmail.com>
2016-08-02 01:57:16 +0530

    Commited!

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template,jsonify
+
+app = Flask(__name__,static_url_path='')
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+@app.route("/_compute_pos")
+def compute():
+	c=0
+	a = request.args.get('a',type=str)
+
+	for i in a:
+		c=c+1
+
+	return jsonify(r=c)
+
+if __name__ == "__main__":
+    app.run(debug=True)
