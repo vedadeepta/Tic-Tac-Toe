8012e49f5d8b4de5deaf535462710f76b75dfbce
veda <pal.veda.deepta@gmail.com>
2016-08-09 02:32:05 +0530

    commited

--- static/javascript/hover.js
+++ static/javascript/hover.js
@@ -12,8 +12,19 @@ jQuery(document).ready(function(){
 		if( $(selector).attr("class") == "elem" )
 		{
 			$(selector).addClass(cl);
-			$(".elem.circle span").css("border-width", $(".elem.circle span").width()/4 | 0 + "px" )
+			
 			recent = true;
+
+
+			if(cl === "cross")
+			{
+				$(selector +"."+cl+ " span").css("background","#ec9771");
+			}
+			else
+			{
+				$(selector +"."+cl+ " span").css("border","solid "+"#ec9771");
+				$(selector + ".circle"+ " span").css("border-width", $(selector + ".circle"+ " span").width()/4.2  + "px" )
+			}
 		}
 
 	},function(){
@@ -21,6 +32,10 @@ jQuery(document).ready(function(){
 		var selector = "#" + this.id + " .elem";
 		if(recent == true)
 		{
+			//console.log("got it");
+			$(selector +"."+cl+ " span").css("background","none");
+			$(selector +"."+cl+ " span").css("border","none");
+			$(selector + ".circle"+ " span").css("border-width", "0px" )
 			$(selector).removeClass(cl);
 			recent = false;
 		}
@@ -31,18 +46,21 @@ jQuery(document).ready(function(){
 		$(".box").toggleClass("uparrow");
 		$(".animate span").css("animation-duration",'0.8s').hide().show(0);
 
-		var selector = "";
 		var index="";
+		var selector = "#" + this.id + " .elem";
 
 		if(recent == true)
 		{
 			recent=false;
 			if(cl === "cross")
 			{
+				$(selector +"."+cl+ " span").css("background","#d0490d");
 				cl="circle";
 			}
 			else
 			{
+				$(selector +"."+cl+ " span").css("border","solid "+"#d0490d");
+				$(selector + ".circle"+ " span").css("border-width", $(selector + ".circle"+ " span").width()/4.2 + "px" )
 				cl="cross";
 			}
 	    }
--- static/javascript/responsive.js
+++ static/javascript/responsive.js
@@ -4,15 +4,20 @@ jQuery(document).ready(function(){
 	//height of wach column depends on font-size;
 
 	$(".row").css("height",($(window).width()/9.10) + "px");
-	$(".elem.circle span").css("border-width", $(".elem.circle span").width()/3.5 + "px" )
+	$(".elem.circle span").css("border-width", $(".elem.circle span").width()/4.2+ "px" )
 	
 	$(".animate span").not("#mid").css("width",$(".animate").width()/5.117 + "px");
 	$(".animate span").eq(2). css("width",$(".animate").width()/3.0702 + "px");
 
 	$(window).resize(function(){
 
-		//adjusting border-width of the circles		
-		$(".elem.circle span").css("border-width", $(".elem.circle span").width()/3.5 + "px" )
+		//adjusting border-width of the circles	
+		for(var i=0; i<3; i++)//loop for workaround, because after resizing $(".elem.circle span").width() was returng 0
+		{
+			$(".elem.circle span").css("border-width", $(".elem.circle span").width()/4.2+ "px" )
+		}
+		//$(".elem.circle span").css("border-width", $(".elem.circle span").width()/3.5+ "px" )
+		//console.log($(".elem.circle span").width()/4.3 | 0);
 
 		//adjusting height of the row
 		$(".row").css("height",($(window).width()/9.10) + "px");
